43b5abe2c0462ca227ff3f07946f8839ffdeea7e
insoo <2846130>
2021-11-22 17:12:20 +0900

    first Commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View, Button } from 'react-native'
+
+const App = () => {
+    return (
+        <View>
+            <Text>123</Text>
+        </View>
+    )
+}
+
+export default App
