2fa0aed7b0e4dbc334b541fbdcb89757873462c2
yrkang <yrkang@nkia.co.kr>
2021-11-24 18:39:27 +0900

    #2 circle button 스타일 적용중
    - on/off svg 이미지 추가
    - metro config 설정 변경
    - package 설치
    - circle button 구조 변경

--- src/index.js
+++ src/index.js
@@ -10,65 +10,68 @@ import {
 import { fontSizeSet } from './size'
 import { colorSet } from './colors'
 import PropTypes from 'prop-types'
-import { ToggleButtonContainer, ToggleButtonWrap } from './style'
+import {
+    ToggleButtonContainer,
+    ToggleButtonInner,
+    ToggleButtonTitle,
+    ToggleButtonWrap,
+} from './style'
+import { SvgUri } from 'react-native-svg'
 import { Platform } from 'react-native'
+import PowerOff from '../assets/powerOff.svg'
 
 export default function CircleButton(props) {
     const {
         title,
         titleStyle,
         buttonStyle,
-        type = 'solid',
-        loading = false,
-        loadingStyle,
-        containerStyle,
-        icon,
-        iconPosition = 'top',
-        iconSize = 20,
-        iconContainerStyle,
+        shape = 'circle',
         disabled,
-        disabledStyle,
-        disabledTitleStyle,
+        color,
+        onPress,
+        width,
+        height,
     } = props
     const [status, setStatus] = React.useState(false)
 
     return (
         <ToggleButtonWrap>
-            <ToggleButtonContainer>
-                <TouchableOpacity
+            <ToggleButtonContainer
+                style={{
+                    borderRadius: 100,
+                    height: 100,
+                    width: 100,
+                    elevation: 5,
+                }}
+                onPress={() => {
+                    setStatus(!status)
+                }}
+                disabled={disabled}
+            >
+                <ToggleButtonInner
                     style={{
+                        width: 85,
+                        height: 85,
                         borderRadius: 100,
-                        fontSize: fontSizeSet.base,
-                        height: 100,
-                        width: 100,
-                        backgroundColor: '#eee',
+                        borderColor: status ? '#ff8585' : '#87e3a6',
+                        borderWidth: 1.5,
+                        elevation: 5,
+                        shadowColor: status ? '#ff3535' : '#1ACA5C',
                         alignItems: 'center',
                         justifyContent: 'center',
-                        elevation: 5,
-                        borderColor: colorSet.white,
-                        borderWidth: 2,
-                    }}
-                    onPress={() => {
-                        setStatus(!status)
+                        backgroundColor: colorSet.white,
                     }}
                 >
-                    <View
-                        style={{
-                            width: 85,
-                            height: 85,
-                            borderRadius: 100,
-                            borderColor: status ? 'red' : '#1ACA5C',
-                            borderWidth: 1.5,
-                            alignItems: 'center',
-                            justifyContent: 'center',
-                            backgroundColor: colorSet.white,
-                        }}
-                    ></View>
-                </TouchableOpacity>
-                <Text style={{ marginTop: 10, fontSize: fontSizeSet.base }}>
-                    {status ? '작업중지' : '작업시작'}
-                </Text>
+                    <PowerOff
+                        width={40}
+                        height={40}
+                        fill={status ? 'red' : '#1ACA5C'}
+                    />
+                </ToggleButtonInner>
             </ToggleButtonContainer>
+            <ToggleButtonTitle>
+                {status ? '작업중지' : '작업시작'}
+            </ToggleButtonTitle>
         </ToggleButtonWrap>
     )
 }
@@ -77,7 +80,7 @@ CircleButton.propTypes = {
     title: PropTypes.string,
     titleStyle: PropTypes.oneOfType([PropTypes.object, PropTypes.node]),
     buttonStyle: PropTypes.oneOfType([PropTypes.object, PropTypes.node]),
-    type: PropTypes.oneOf(['solid' | 'clear' | 'outline']),
+    shape: PropTypes.oneOf(['circle' | 'square']),
     loading: PropTypes.bool,
     loadingStyle: PropTypes.oneOfType([PropTypes.object, PropTypes.node]),
     containerStyle: PropTypes.oneOfType([PropTypes.object, PropTypes.node]),
--- src/style.js
+++ src/style.js
@@ -7,13 +7,25 @@ import { fontSizeSet } from './size'
 import { colorSet } from './colors'
 
 export const ToggleButtonWrap = styled.View`
-    border: 1px solid red;
+    align-items: center;
 `
 ToggleButtonWrap.displayName = 'ToggleButtonWrap'
 
-export const ToggleButtonContainer = styled.View`
-    border: 1px solid blue;
+export const ToggleButtonContainer = styled.TouchableOpacity`
+    font-size: ${fontSizeSet.base}px;
+    background: ${colorSet.disableBg};
     align-items: center;
-    width: 100px;
+    justify-content: center;
+    border: 2px solid ${colorSet.white};
 `
 ToggleButtonContainer.displayName = 'ToggleButtonContainer'
+
+export const ToggleButtonInner = styled.Text``
+ToggleButtonInner.displayName = 'ToggleButtonInner'
+
+export const ToggleButtonTitle = styled.Text`
+    margin-top: 10px;
+    font-size: ${fontSizeSet.base}px;
+    color: ${colorSet.normalTextColor};
+`
+ToggleButtonTitle.displayName = 'ToggleButtonTitle'
